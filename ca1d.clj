58707751f5db0871cf57a9cdd3cb60ee038aa153
Tomohiro Moro <arksutite@gmail.com>
2012-04-18 01:47:01 +0900

    使えそうな関数を使ってすっきりさせた

--- ca1d.clj
+++ ca1d.clj
@@ -1,25 +1,19 @@
-(def num-trial 32)
+(def trials 32)
 (def rule 90)
-(def initial-state (flatten (list (repeat 32 0) 1 (repeat 32 0))))
+(def initial-state (concat (repeat 32 0) '(1) (repeat 32 0)))
 
 (defn show-cells [cells]
   (println (apply str (map #(cond (= % 0) " "
                                   (= % 1) "*") cells))))
 
-(defn neighbor [idx source]
-  (if (= idx 0)
-    (cons (last source) (first (split-at 2 source)))
-    (first (split-at 3 (last (split-at (- idx 1) (cycle source)))))))
-
 (defn neighbors [state]
-  (map #(neighbor % state) (take (count state) (iterate inc 0))))
-
-(defn pairs [seq]
-  (if (> (count seq) 1)
-    (cons (take 2 seq) (pairs (last (split-at 2 seq))))))
+  (concat
+    (list (cons (last state) (first (split-at 2 state))))
+    (partition 3 1 state)
+    (list (concat (last (split-at (- (count state) 2) state)) (list (first state))))))
 
 (defn pattern [n]
-  (reduce + (map #(apply * %) (pairs (interleave [4 2 1] n)))))
+  (reduce + (map #(apply * %) (partition 2 (interleave [4 2 1] n)))))
 
 (defn next-status [pat]
   (if (> (bit-and rule (bit-shift-left 1 pat)) 0) 1 0))
@@ -27,5 +21,5 @@
 (defn tick [state]
   (map next-status (map pattern (neighbors state))))
 
-(doseq [state (take num-trial (iterate tick initial-state))]
+(doseq [state (take trials (iterate tick initial-state))]
   (show-cells state))
